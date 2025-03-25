048734318739e450a3d86cafa1174782adec133e
Erik Voigt <erik.voigt@gmail.com>
2025-03-25 15:38:35 -0400

    player created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     
@@ -10,13 +11,31 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(COLOR_BLACK)
+        
+        screen.fill("black")
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
 
 if __name__ == "__main__":
     main()
